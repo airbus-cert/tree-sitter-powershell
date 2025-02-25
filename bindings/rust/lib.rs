bc38714d068f9b4ca20f4edde6b483bb54cff713
William Woodruff <william@yossarian.net>
2025-02-25 16:47:42 -0500

    use tree-sitter-language APIs
    Signed-off-by: William Woodruff <william@yossarian.net>

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_powershell::language()).expect("Error loading powershell grammar");
+//! parser.set_language(&tree_sitter_powershell::LANGUAGE.into()).expect("Error loading powershell grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -15,18 +15,16 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_powershell() -> Language;
+    fn tree_sitter_powershell() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_powershell() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_powershell) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -46,7 +44,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::LANGUAGE.into())
             .expect("Error loading powershell language");
     }
 }
